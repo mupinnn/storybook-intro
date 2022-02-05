3c21e6ca412d6e07a8265e794636e249f78b3688
Ahmad Muwaffaq <itsmupin@gmail.com>
2022-02-05 15:40:58 +0700

    feat: mock redux in `PureInboxScreen` stories

--- src/components/PureInboxScreen.stories.js
+++ src/components/PureInboxScreen.stories.js
@@ -1,8 +1,30 @@
+import { Provider } from "react-redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { PureInboxScreen } from "./InboxScreen";
+import * as TaskListStories from "./TaskList.stories";
+
+const MockStore = configureStore({
+  reducer: {
+    tasks: createSlice({
+      name: "tasks",
+      initialState: TaskListStories.Default.args.tasks,
+      reducers: {
+        updateTaskState: (state, action) => {
+          const { id, newTaskState } = action.payload;
+          const task = state.findIndex((task) => task.id === id);
+          if (task >= 0) {
+            state[task].state = newTaskState;
+          }
+        },
+      },
+    }).reducer,
+  },
+});
 
 export default {
   component: PureInboxScreen,
   title: "PureInboxScreen",
+  decorators: [(story) => <Provider store={MockStore}>{story()}</Provider>],
 };
 
 const Template = (args) => <PureInboxScreen {...args} />;
