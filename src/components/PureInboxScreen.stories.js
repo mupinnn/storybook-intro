a3da3030265c1e81462d4e10de89bf62f77d4c5f
Ahmad Muwaffaq <itsmupin@gmail.com>
2022-02-05 15:39:59 +0700

    feat: `PureInboxScreen` component

---
+++ src/components/PureInboxScreen.stories.js
@@ -0,0 +1,15 @@
+import { PureInboxScreen } from "./InboxScreen";
+
+export default {
+  component: PureInboxScreen,
+  title: "PureInboxScreen",
+};
+
+const Template = (args) => <PureInboxScreen {...args} />;
+
+export const Default = Template.bind({});
+
+export const Error = Template.bind({});
+Error.args = {
+  error: "Something went wrong",
+};
